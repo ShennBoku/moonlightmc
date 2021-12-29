1d75ad3727f8f9e3bebedf95c6195fa265a46bfc
Afdhalul Ichsan Yourdan <40620980+ShennBoku@users.noreply.github.com>
2021-12-29 20:48:59 +0700

    Update serverstatus.js

--- docs/_includes/serverstatus.js
+++ docs/_includes/serverstatus.js
@@ -9,7 +9,7 @@ function handleErrors(response) {
 }
 
 // Fetch API, mcsrvstat
-fetch("https://api.mcsrvstat.us/{% if site.useBedrockForOnlineStatus == true %}bedrock/{% endif %}2/{{ site.serverIP }}")
+fetch("https://api.mcsrvstat.us/2/49.128.187.70:25612")
     .then(handleErrors)
     .then(response => {
         return response.json();
